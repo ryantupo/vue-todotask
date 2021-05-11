27d7f8e0a64df37b6897f57a7a41a51d9dec7c64
Ryan <ryantupo98@gmail.com>
2021-05-11 15:52:05 +0100

    11.05.2021 setting up task

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,40 @@
 </template>
 
 <script>
-import Header from './components/Header'
+import Header from "./components/Header";
 
 export default {
   name: "App",
   components: {
-    Header,
+    Header
   },
+  data() {
+    return {
+      tasks: []
+    };
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Docotors Apointment",
+        day: "March 1st at 2:30pm",
+        reminder: true
+      },
+      {
+        id: 2,
+        text: "Meeting at School",
+        day: "March 3rd at 1:30pm",
+        reminder: true
+      },
+      {
+        id: 3,
+        text: "Food Shopping",
+        day: "March 3rd at 11:00am",
+        reminder: false
+      }
+    ];
+  }
 };
 </script>
 
---
+++ src/components/Tasks.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Tasks',
+    props:{
+        tasks: Array
+    }
+}
+</script>
