4d235bf14294c118df124b41f12f80aea43bdf55
Ryan <ryantupo98@gmail.com>
2021-05-11 21:53:46 +0100

    11.05.2021 tasks done not to make functional

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,8 +1,9 @@
   
 <template>
-  <div class="task">
+  <div :class="[task.reminder ? 'reminder' : '', 'task']">
     <h3>
       {{ task.text }}
+      <i class="fas fa-times"></i>
     </h3>
     <p>{{ task.day }}</p>
   </div>
@@ -17,5 +18,22 @@ export default {
 };
 </script>
 
-<style >
+<style scope>
+.fas {
+  color: red;
+}
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.task.reminder {
+  border-left: 5px solid green;
+}
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
